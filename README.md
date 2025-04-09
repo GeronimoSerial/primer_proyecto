4f25a8332638db089fe47238e95e65d87502ce9a
Geronimo Serial <serialgeronimo@gmail.com>
2025-04-09 18:19:41 -0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Talentos Digitales 
+
+
+Pagina basada en https://talentosdigitales.ar/
+
+Alumno: Serial Geronimo
+"# PrimerProyecto" 
---
+++ app/Controllers/usuario_controller.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controllers;
+Use App\Models\usuario_model;
+use CodeIgniter\Controller;
+
+class usuario_controller extends Controller{
+    public function __construct(){
+        helper (['form', 'url']);
+
+    }
+
+    public function create(){
+        $dato['titulo']='registro';
+        echo view('front/head_view', $dato);
+        echo view('front/navbar_view');
+        echo view('back/usuario/registro'); 
+        echo view('front/footer_view');
+    } 
+
+    public function formValidation(){
+        $input = $this->validate([
+            'nombre' => 'required|min_length[3]',
+            'apellido' => 'required|min_length[3]|max_length[25]',
+            'usuario' => 'required|min_length[3]',
+            'email' => 'required|min_length[3]|max_length[100]|valid_email|is_unique[usuarios.email]',
+            'pass' => 'required|min_length[3]|max_length[10]'
+        ]);
+    
+        $formModel = new usuario_model();
+        $email = $this->request->getVar('email');
+        
+        // Verificar si el email ya está en uso
+        if ($formModel->where('email', $email)->first()) {
+            session()->setFlashdata('fail', 'El email ya está en uso.');
+        } else {
+            if(!$input){
+                $data['titulo']='registro';
+                echo view('front/head_view', $data);
+                echo view('front/navbar_view');
+                echo view('back/usuario/registro', ['validation' => $this -> validator]);
+                echo view('front/footer_view');
+            }
+            else {
+                $formModel->save([
+                    'nombre' => $this->request->getVar('nombre'),
+                    'apellido' => $this->request->getVar('apellido'),
+                    'usuario' => $this->request->getVar('usuario'),
+                    'email' => $this->request->getVar('email'),
+                    'pass' => password_hash($this->request->getVar('pass'), PASSWORD_DEFAULT)
+                ]);
+    
+                // flashData funciona solo en redirigir la funcion en el controlador en la vista de carga.
+                session()->setFlashdata('success', 'Usuario registrado con exito');
+            }
+        }
+        return $this->response->redirect('registro');
+    }
+}
